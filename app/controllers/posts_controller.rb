7f0b64a11d63f8dc999372e9c0f70b99e2108dea
doctor3397 <hs2546@columbia.edu>
2016-03-03 17:01:06 -0500

    add post new action and form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,24 @@ class PostsController < ApplicationController
 	def show
 		@post = Post.find(params[:id])
 	end
+
+    def new
+    	@post = Post.new
+    end
+
+	def create
+		post = Post.new(post_params)
+		#post.save!
+		if post.save!
+			redirect_to posts_path
+		else
+			render :new
+		end
+	end
+
+	#strong parameter 
+    private
+	def post_params
+    	params.require(:post).permit(:title, :comment)
+    end  
 end
