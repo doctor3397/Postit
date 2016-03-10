f21284b54b6b3c97a982c87827ee8254018aa6a8
doctor3397 <hs2546@columbia.edu>
2016-03-10 18:35:26 -0500

    add user authentication, user table, user register page

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160218175920) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
   end
 
 end
